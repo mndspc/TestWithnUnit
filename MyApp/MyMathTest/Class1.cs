2323055fb2a06d035bed1099dac04e4044f1c411
Shoaib <shoaib.ghachi@gmail.com>
2022-07-24 12:59:26 +0530

    initial commit

---
+++ MyApp/MyMathTest/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MyMathApp;
+namespace MyMathTest
+{
+    [TestFixture]
+    public class MathTest
+    {
+        [Test]
+        [TestCase(10,30)]
+        public void AddTest(int num1,int num2)
+        {
+            MyMath myMath = new MyMath();
+           var actualResult= myMath.Add(num1,num2);
+            var expectedResult = num1 + num2;
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
